a21ca5e658076660a572fe6a5313bb0b99533dd9
Stephen Turcol <stephen.turcol@sas.com>
2025-09-26 00:03:40 -0400

    test

--- template/viyaDeploy.sh
+++ template/viyaDeploy.sh
@@ -2251,10 +2251,7 @@ if [ "${IS_UPDATE}" == "True" ]; then
   fi
   echolog "Updating current AKS API server authorized IP ranges to: $MERGED"
 
-  az aks update \
-    --resource-group "$RG" \
-    --name "$AKS_NAME" \
-    --api-server-authorized-ip-ranges "$MERGED" >>$LOGFILE 2>&1
+  az aks update -g ${RG} -n ${AKS} --api-server-authorized-ip-ranges "${MERGED}" >>$LOGFILE 2>&1
 
   echolog "Successfully updated AKS API server authorized IP ranges."
 fi
